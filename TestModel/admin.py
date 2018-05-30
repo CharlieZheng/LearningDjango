041c108f3e88f7eef793c2d757a128efe80dada3
东莞市浙金信息科技有限公司Ubuntu <740483802@qq.com>
2018-05-30 16:43:41 +0800

    Finished vote project

--- TestModel/admin.py
+++ TestModel/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
-admin.site.register(Question)
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    """使用admin.TabularInline替代admin.StackedInline以达到更好的显示效果"""
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    """Indicate show form of model"""
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
